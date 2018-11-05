22c820c091b494fa3b32100f5c9e945a788ebc55
marcoslimaprogram <44383912+marcoslimaprogram@users.noreply.github.com>
2018-11-05 20:42:03 -0200

    Add files via upload

---
+++ Aula16/16_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class Teste {
+	public static void main(String[] args) {
+		boolean b = false;
+		boolean a = true;
+		boolean c = a && !b;
+		if (c);
+		
+		if (a == b)
+		if (a != b) {
+			
+			System.out.println("Iguais");
+			
+		} else {
+
+			System.out.println("Diferentes");
+		}
+	}
+
+}
